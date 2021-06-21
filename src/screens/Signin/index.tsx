00a9024b89fb3478dfffc73e25110992a3343133
Alessandro <alessandrocruzadma01@gmail.com>
2021-06-21 01:16:28 -0500

    tela home init

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,51 @@
+// Renderizacao de componentes
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    Image,
+    StatusBar
+} from 'react-native'
+
+import IllustrationImg from '../../assets/illustration.png'
+import { styles } from './styles'
+
+export function SignIn() { //Neste caso nao usamos o defaut, pois quando formos importar por chaves!
+    // Criando um estado!
+    // const [text, setText] = useState('Alessandro'); //passamos [nomeDoEstado, funcaoQueAtualizaEsseEstado] = useState(<valorInicial>)
+
+    return (
+        <View style={styles.container}>
+
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode='stretch'
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {'\n'}
+                    e organize suas {'\n'}
+                    jogatinas {'\n'}
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos {'\n'}
+                </Text>
+
+
+
+            </View>
+
+        </View >
+    );
+}
