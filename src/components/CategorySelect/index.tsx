65e1296c93f30d1a05bfd6603207a16755252b25
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-06-22 15:44:30 -0500

    finalizacao do category select

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+
+import { styles } from "./styles";
+import { categories } from "../../utils/categories";
+
+import { Category } from "../Category";
+
+type Props = {
+    categorySelected: string
+    setCategory: (categoryId: string) => void  //Em outras palavras, por parametro as propriedades de CategorySelected, temos uma striong com o nome da categoria e uma funcao que recebe o id de uma categoria mas n retorna nada
+}
+
+export function CategorySelect({ categorySelected, setCategory }: Props) {
+    return (
+        <ScrollView
+            horizontal
+            style={styles.container}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingRight: 40 }}
+        >
+            {
+                categories.map(category => (
+                    <Category
+                        key={category.id}
+                        title={category.title}
+                        icon={category.icon}
+                        checked={category.id === categorySelected}
+                        onPress={() => setCategory(category.id)}
+                    />
+                ))
+
+            }
+        </ScrollView >
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,16 @@ import { View } from 'react-native'
 import { Profile } from "../../components/Profile";
 import { styles } from "./styles";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
+import { useState } from "react";
 
 export function Home() {
+    const [category, setCategory] = useState('')
+
+    function handleCategorySelect(categoryId: string) {
+        categoryId === category ? setCategory('') : setCategory(categoryId)  //Ternario para testar, categoryId é true? ou seja , o icone esta selecionado? se sim desmarca, se nao marca com o id da opcao selecionada
+    }
+
     return (
         <View>
 
@@ -12,6 +20,14 @@ export function Home() {
                 <Profile />
                 <ButtonAdd />
             </View>
+
+            <View>
+                <CategorySelect
+                    categorySelected={category}
+                    setCategory={handleCategorySelect}
+                />
+            </View>
+
 
         </View>
     )
