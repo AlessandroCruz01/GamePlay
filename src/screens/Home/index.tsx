cb2277cd3cbed077025f863203558f6a110f35e0
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-06-22 17:33:52 -0500

    list apointements

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { View } from 'react-native'
+import {
+    View,
+    FlatList,
+    Text,
+} from 'react-native'
 import { Profile } from "../../components/Profile";
+import { ListHeader } from "../../components/ListHeader";
 import { styles } from "./styles";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 import { useState } from "react";
+import { Appointments } from "../../components/Appointments";
 
 export function Home() {
     const [category, setCategory] = useState('')
+
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 ás 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        }
+    ]
 
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId)  //Ternario para testar, categoryId é true? ou seja , o icone esta selecionado? se sim desmarca, se nao marca com o id da opcao selecionada
@@ -26,6 +47,24 @@ export function Home() {
                     categorySelected={category}
                     setCategory={handleCategorySelect}
                 />
+            </View>
+
+            <View style={styles.content}>
+
+                <ListHeader
+                    title="Partidas Agendadas"
+                    subtitle="Total 6"
+                />
+
+                <FlatList
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <Appointments data={item} />
+                    )}
+                />
+
+
             </View>
 
 
