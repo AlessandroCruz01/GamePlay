93800cc9468132804683d895d33b221c6c27645b
Alessandro <alessandrocruzadma01@gmail.com>
2021-06-21 23:09:26 -0500

    iniciando a tela home, estilizando avatar

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
 import AppLoading from "expo-app-loading";
 
-import { SignIn } from './src/screens/Signin/index'
+// import { SignIn } from './src/screens/Signin/index'
+import { Home } from "./src/screens/Home";
 import { Background } from './src/components/Background'
 
 
@@ -31,7 +32,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
+      <Home />
     </Background>
   );
 }
