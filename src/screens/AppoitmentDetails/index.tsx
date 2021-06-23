52d4d97a6ad2d0e10c13c6d0dcca98ee2152a42f
Alessandro <alessandrocruzadma01@gmail.com>
2021-06-23 00:07:10 -0500

    Imagem de fundo com titulo a frente

--- src/screens/AppoitmentDetails/index.tsx
+++ src/screens/AppoitmentDetails/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 import { styles } from './styles'
 import { Background } from '../../components/Background'
 import { Header } from "../../components/Header";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { Fontisto } from "@expo/vector-icons";
 import { theme } from "../../global/styles/theme";
+import BannerImg from '../../assets/banner.png'
 
 export function AppoitmentDetails() {
     return (
@@ -22,6 +23,26 @@ export function AppoitmentDetails() {
                     </BorderlessButton>
                 }
             />
+
+            <ImageBackground
+                source={BannerImg}
+                style={styles.banner}
+            >
+                <View style={styles.bannerContent}>
+
+                    <Text style={styles.title}>
+                        Lendários
+                    </Text>
+
+                    <Text style={styles.subtitle}>
+                        É hoje que vamos chegar ao challenger sem perder uma partida da md10
+                    </Text>
+
+                </View>
+
+
+
+            </ImageBackground>
         </Background>
     )
 
