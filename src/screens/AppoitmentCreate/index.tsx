3d31c646824f795a6ccc2745082598a3f0105bf5
Alessandro <alessandrocruzadma01@gmail.com>
2021-06-26 03:27:45 -0500

    Realizando agendamentos

--- src/screens/AppoitmentCreate/index.tsx
+++ src/screens/AppoitmentCreate/index.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { RectButton } from 'react-native-gesture-handler'
 import { Feather } from "@expo/vector-icons";
+import uuid from 'react-native-uuid'
 
 import { styles } from './styles'
 
@@ -24,6 +25,9 @@ import { Button } from "../../components/Button";
 import { Guilds } from "../Guilds";
 import { GuildProps } from "../../components/Guild";
 import { Background } from "../../components/Background";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { COLLECTION_APPOINTMENTS } from "../../config/database";
+import { useNavigation } from "@react-navigation/native";
 
 
 
@@ -31,6 +35,15 @@ export function AppoitmentCreate() {
     const [category, setCategory] = useState('')
     const [openGuildsModal, setOpenGuildsModal] = useState(false)
     const [guild, setGuild] = useState<GuildProps>({} as GuildProps)
+
+    const [day, setDay] = useState('')
+    const [month, setMonth] = useState('')
+    const [hour, setHour] = useState('')
+    const [minute, setMinute] = useState('')
+    const [description, setDescription] = useState('')
+
+    const navigation = useNavigation()
+
 
     function handleOpenGuilds() {
         setOpenGuildsModal(true)
@@ -48,6 +61,27 @@ export function AppoitmentCreate() {
 
     function handleCategorySelect(categoryId: string) {
         setCategory(categoryId)  //Ternario para testar, categoryId é true? ou seja , o icone esta selecionado? se sim desmarca, se nao marca com o id da opcao selecionada
+    }
+
+    async function handleSave() {
+        const newAppoitment = {
+            id: uuid.v4(),
+            guild,
+            category,
+            date: `${day}/${month} às ${hour}:${minute}h`,
+            description
+        }
+
+        const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
+        const appointments = storage ? JSON.parse(storage) : []
+
+        await AsyncStorage.setItem(
+            COLLECTION_APPOINTMENTS,
+            JSON.stringify([...appointments, newAppoitment])
+        )
+
+        navigation.navigate('Home')
+
     }
 
     return (
@@ -110,11 +144,17 @@ export function AppoitmentCreate() {
                                     Dia e Mês
                                 </Text>
                                 <View style={styles.column}>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setDay}
+                                    />
                                     <Text style={styles.divider}>
                                         /
                                     </Text>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setMonth}
+                                    />
                                 </View>
                             </View>
 
@@ -123,11 +163,17 @@ export function AppoitmentCreate() {
                                     Horario
                                 </Text>
                                 <View style={styles.column}>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setHour}
+                                    />
                                     <Text style={styles.divider}>
                                         :
                                     </Text>
-                                    <SmallInput maxLength={2} />
+                                    <SmallInput
+                                        maxLength={2}
+                                        onChangeText={setMinute}
+                                    />
                                 </View>
                             </View>
 
@@ -150,10 +196,14 @@ export function AppoitmentCreate() {
                             maxLength={100}
                             numberOfLines={5}
                             autoCorrect={false}
+                            onChangeText={setDescription}
                         />
 
                         <View style={styles.footer}>
-                            <Button title="Agendar" />
+                            <Button
+                                title="Agendar"
+                                onPress={handleSave}
+                            />
                         </View>
 
                     </View>
